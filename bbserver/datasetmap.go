0495d26268b19f13aa5d08f14bd1572fbafa6398
karagatandev <alex@karagatan.com>
2018-11-07 21:00:05 -0800

    do not return nil values in client API

--- bbserver/datasetmap.go
+++ bbserver/datasetmap.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018-present Alexander Shvid and other authors
+ * Copyright 2018-present Alexander Shvid and Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
